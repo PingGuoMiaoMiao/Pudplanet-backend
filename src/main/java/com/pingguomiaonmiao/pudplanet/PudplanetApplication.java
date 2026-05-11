b0ed82591a78bb3feaa21446310266cc08d1697f
苹果喵喵 <3226742838@qq.com>
2026-05-11 12:57:39 +0800

    first commit

---
+++ src/main/java/com/pingguomiaonmiao/pudplanet/PudplanetApplication.java
@@ -0,0 +1,13 @@
+package com.pingguomiaonmiao.pudplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PudplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PudplanetApplication.class, args);
+	}
+
+}
